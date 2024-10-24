afd303d93cfed9397dea3aac757a945c69db8a11
Emilly Oliveira <eoliveiraa2007@gmail.com>
2024-10-24 16:54:30 -0300

    estudando

---
+++ Arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string [] carros = new string[3];
+
+
+for(int i = 0; i < carros.Leight; i++ )
+{
+
+Console.WriteLine($"Digite o nome do carro a ser cadastrado:");
+carros[i] = Console.ReadLine()!;
+
+}
+
+Console.WriteLine($"O carro cadastrado e {carros[0]}, {carros[1]}, {carros[2]}");
+
