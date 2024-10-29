46b8d531ac56a987253f30ae85363cd6e0e21c7e
Emilly Oliveira <eoliveiraa2007@gmail.com>
2024-10-29 15:45:34 -0300

    Foreach

---
+++ Foreach/DesafioForeach/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] numeros = {10,20,30,40,50,60,70,90,100};
+
+foreach ( int n in numeros)
+{
+   Console.WriteLine($"O dobro dos numeros eh:{n*2}");
+}
---
+++ Foreach/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] frutas = {"Morango", "Limao", "Caju", "Manga", "Abacate", "Pitaya", "Jabuticaba", "Kiwi","Pessego", "Banana", "Acerola"};
+
+Console.WriteLine($"Sua sacola contem {frutas.Length} frutas!");
+
+foreach(string f in frutas)
+{
+    Console.WriteLine($"{f},");
+}
